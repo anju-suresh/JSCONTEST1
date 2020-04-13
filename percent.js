0ff3e4329f623068f287ae0e09ae7197641c433d
anju-suresh <ksanju514@gmail.com>
2020-04-13 15:55:01 +0530

    completed

---
+++ percent.js
@@ -0,0 +1,35 @@
+var y=document.getElementsByClassName("cards");
+y.setAttribute("hidden","true");
+function calcPercent(){
+   var name =document.getElementById("nam").value; 
+   var clas =document.getElementById("cls").value;   
+   var mark1=parseInt(document.getElementById("mark1").value);
+   var mark2=parseInt(document.getElementById("mark2").value);
+   var mark3=parseInt(document.getElementById("mark3").value);
+//    var y=document.getElementsByClassName("cards");
+//    y.setAttribute("hidden","true");
+      if(isNaN(mark1)||isNaN(mark2)||isNaN(mark3))
+     {
+        persent="Can not find the persentage"
+    }
+    else{
+        var total= mark1+mark2+mark3;
+        var persent =parseInt((total*100)/300);
+        if(persent<=40){
+            
+            document.getElementById("result").style.color="red";
+        }
+        else if(persent<=70){
+            document.getElementById("result").style.color="orange";
+        }
+        else{
+            document.getElementById("result").style.color="Green";
+        }
+        
+        
+    }
+    document.getElementById("one").innerHTML="The Percentage obtained by your ward "+name+" of class "+clas+" has scored";
+    document.getElementById("result").innerHTML=persent;
+
+}
+        
